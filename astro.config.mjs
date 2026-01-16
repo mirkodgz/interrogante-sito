477cd085a51389db59fc37377ef2a42887d944fa
mirkodgz <mirkodgz@users.noreply.github.com>
2026-01-16 18:25:13 +0100

    Fix: update site URL to match vercel domain for OAuth

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://interrogantespettacolo.com',
+  site: 'https://interrogante-sito.vercel.app',
   output: 'server',
   integrations: [react(), keystatic(), markdoc(), sitemap()],
 
